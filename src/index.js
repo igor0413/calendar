28f14f0778609a14fb8c29a4547bbeddaeeb9281
Igor_Sysoev <Igor_Sysoev@epam.com>
2018-01-11 16:42:04 +0300

    routes configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom'
 import {App} from './components/App'
+import {Router, Route, hashHistory} from 'react-router'
+import {Whoops404} from './components/Whoops404'
 
 window.React = React
 
 render (
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="list-days" component={App}>
+      <Route path=":filter"></Route>
+    </Route>
+    <Route path="add-day" component={App}/>
+    <Route path="*" component={Whoops404}/>
+  </Router>,
   document.getElementById('react-container')
 )
 
